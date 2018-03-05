50adb7ab280c96b5a75292b4b89e93c562ee2035
Denis Doskach <denisdoskach@gmail.com>
2018-03-05 21:36:56 +0200

    Implemented balance functional

--- AVLTree/AVLTree/Tree.h
+++ AVLTree/AVLTree/Tree.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <functional>
+#include <algorithm>
 
 template<typename val_type, typename comp_type=std::less<val_type>>
 class Tree
@@ -17,8 +18,61 @@ private:
 
 public:
 
+
 private:
 	comp_type pred;
 	Node* root;
 
+	size_t height(Node* p)const
+	{
+		return p != nullptr ? p->height : 0;
+	}
+	void reset_height(Node* p)
+	{
+		p->height = std::max(p->left->height, p->right->height) + 1;
+	}
+	int balance_factor(Node* p)
+	{
+		return height(p->right) - height(p->left);
+	}
+
+	Node* rotate_left(Node* root)
+	{
+		Node* new_root = root->right;
+		root->right = new_root->left;
+		new_root->left = root;
+		reset_height(root);
+		reset_height(new_root);
+		return new_root;
+	}
+	Node* rotate_right(Node* p)
+	{
+		Node* new_root = root->left;
+		root->left = new_root->right;
+		new_root->right = root;
+		reset_height(root);
+		reset_height(new_root);
+		return new_root;
+	}
+	Node* balance(Node* root)
+	{
+		reset_height(root);
+		if(balance_factor(root)==2)
+		{
+			if (balance_factor(root->right) < 0)
+			{
+				root->right = rotate_right(root->right);
+			}
+			return rotate_left(root);
+		}
+		if(balance_factor(root)==-2)
+		{
+			if(balance_factor(root->left)>0)
+			{
+				root->left = rotate_left(root->left);
+			}
+			return rotate_right(root);
+		}
+		return root;
+	}
 };
