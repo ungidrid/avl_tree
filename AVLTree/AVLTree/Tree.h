91b0948d72dfc63338a3c26d3290f46bf9a2c3f3
Denis Doskach <denisdoskach@gmail.com>
2018-03-05 21:25:41 +0200

    Implemented base structure of tree

--- AVLTree/AVLTree/Tree.h
+++ AVLTree/AVLTree/Tree.h
@@ -7,6 +7,18 @@ class Tree
 private:
 	struct Node
 	{
-		
+		val_type key;
+		size_t height = 1;
+		Node* left = nullptr;
+		Node* right = nullptr;
+
+		Node(const val_type& k) :key(k){}
 	};
+
+public:
+
+private:
+	comp_type pred;
+	Node* root;
+
 };
